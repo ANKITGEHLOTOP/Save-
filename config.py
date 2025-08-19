884dcb3c1c548f9dfcffad7b97fef107803aa3e0
ANKITGEHLOTOP <technicalankit123@gmail.com>
2025-08-19 12:44:05 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# devgagan
+# Note if you are trying to deploy on vps then directly fill values in ("")
+
+from os import getenv
+
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://dafeh64648:/?retryWrites=true&w=majority&appName=Ramsingh")
+LOG_GROUP = getenv("LOG_GROUP", "")
+CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "100"))
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "10000"))
+WEBSITE_URL = getenv("WEBSITE_URL", "gplinks.com")
+AD_API = getenv("AD_API", "")
+STRING = getenv("STRING", None)
+YT_COOKIES = getenv("YT_COOKIES", None)
+INSTA_COOKIES = getenv("INSTA_COOKIES", None)
